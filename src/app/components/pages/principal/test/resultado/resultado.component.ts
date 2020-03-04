6e772c1daa4b951a30268be21a72f06cca73f863
erik <erik19970805@gmail.com>
2020-03-04 15:49:06 -0500

    a

--- src/app/components/pages/principal/test/resultado/resultado.component.ts
+++ src/app/components/pages/principal/test/resultado/resultado.component.ts
@@ -56,11 +56,16 @@ export class ResultadoComponent implements OnInit {
   }
 
   generarPDF() {
-    html2canvas(document.body).then(function (canvas) {
-      let img = canvas.toDataURL("image/png");
-      let doc = new jsPDF();
-      doc.addImage(img, 'JPEG', 15, 15);
-      doc.save(`${this.id}.pdf`);
+    html2canvas(document.body).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF({
+        orientation: 'landscape',
+      });
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('test.pdf');
     });
   }
 }
