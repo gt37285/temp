144047cd0152946c72760db60b22cd978c73df0d
erik <erik19970805@gmail.com>
2020-03-04 11:01:41 -0500

    agregacion del pdf

--- src/app/components/pages/principal/test/resultado/resultado.component.ts
+++ src/app/components/pages/principal/test/resultado/resultado.component.ts
@@ -2,26 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { evaluacionesService } from 'src/app/services/evaluaciones.service';
 import { UsuariosService } from 'src/app/services/usuarios.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resultado',
   templateUrl: './resultado.component.html',
-  styleUrls: ['./resultado.component.scss','../test.component.scss']
+  styleUrls: ['./resultado.component.scss', '../test.component.scss']
 })
 export class ResultadoComponent implements OnInit {
 
-  private id:String = ''
-  public loading:Boolean = false
-  public evaluaciones:any = []
-  public resultados:any 
+  private id: String = ''
+  public loading: Boolean = false
+  public evaluaciones: any = []
+  public resultados: any
 
   constructor(
-    private router:Router,
-    private active_router:ActivatedRoute,
+    private router: Router,
+    private active_router: ActivatedRoute,
     private service: evaluacionesService,
-    public userService:UsuariosService
-  ) { 
-    this.active_router.params.subscribe((data:any) => this.id = data.id)
+    public userService: UsuariosService
+  ) {
+    this.active_router.params.subscribe((data: any) => this.id = data.id)
   }
 
   ngOnInit() {
@@ -29,11 +31,11 @@ export class ResultadoComponent implements OnInit {
     this.cargarResultados()
   }
 
-  cargarEvaluacion(){
+  cargarEvaluacion() {
     this.loading = true
     this.service.listarPorId(this.id)
       .subscribe(
-        ((data:any) => {
+        ((data: any) => {
           this.evaluaciones = data.evaluacion
           this.loading = false
         }),
@@ -41,11 +43,11 @@ export class ResultadoComponent implements OnInit {
       )
   }
 
-  cargarResultados(){
+  cargarResultados() {
     this.loading = true
     this.service.resultados(this.id)
       .subscribe(
-        ((data:any) => {
+        ((data: any) => {
           this.resultados = data.estudiante
           this.loading = false
         }),
@@ -53,4 +55,12 @@ export class ResultadoComponent implements OnInit {
       )
   }
 
+  generarPDF() {
+    html2canvas(document.body).then(function (canvas) {
+      let img = canvas.toDataURL("image/png");
+      let doc = new jsPDF();
+      doc.addImage(img, 'JPEG', 15, 15);
+      doc.save(`${this.id}.pdf`);
+    });
+  }
 }
--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 const URL_SERVICES =
     // "https://vinculacion.herokuapp.com"
-    "http://localhost:3000"
-
+    //"http://localhost:3000"
+    `http://68.183.115.51:3000`
 export { URL_SERVICES };
